460371d340420f1236bc7e8f92dc0a694c3130ee
vctt94 <victor.duarte.oliveira@gmail.com>
2017-05-03 12:57:38 -0300

    finishing user crud

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -2,8 +2,9 @@
 'use strict';
 
 
-const mongoose = require('mongoose'),
-  	  User = mongoose.model('User')
+const _ 	   = require('lodash'),
+	  mongoose = require('mongoose'),
+  	  User 	   = mongoose.model('User')
 
 
 const userController = {
@@ -38,6 +39,74 @@ const userController = {
 	        next()
 
 	    })
+	},
+	getUserById : function(req, res, next) {
+
+	    User.findOne({ _id: req.params.user_id }, function(err, doc) {
+
+	        if (err) {
+	            log.error(err)
+	            return next(new errors.InvalidContentError(err.errors.name.message))
+	        }
+
+	        res.send(doc)
+	        next()
+
+	    })
+
+	},
+	updateUserById : function(req, res, next) {
+
+	    let data = req.body || {}
+
+	    console.log(data)
+
+	    if (!data._id) {
+			_.extend(data, {
+				_id: req.params.user_id
+			})
+		}
+
+	    User.findOne({ _id: req.params.user_id }, function(err, doc) {
+
+			if (err) {
+				log.error(err)
+				return next(new errors.InvalidContentError(err.errors.name.message))
+			} else if (!doc) {
+				return next(new errors.ResourceNotFoundError('The resource you requested could not be found.'))
+			}
+
+			User.update({ _id: data._id }, data, function(err) {
+
+
+				if (err) {
+					log.error(err)
+					return next(new errors.InvalidContentError(err.errors.name.message))
+				}
+
+
+				res.send(200, data)
+	            next()
+
+			})
+
+		})
+
+	},
+	deleteUserById : function(req, res, next) {
+
+	    User.remove({ _id: req.params.user_id }, function(err) {
+
+			if (err) {
+				log.error(err)
+				return next(new errors.InvalidContentError(err.errors.name.message))
+			}
+
+			res.send(204)
+	        next()
+
+		})
+
 	}
 }
 
