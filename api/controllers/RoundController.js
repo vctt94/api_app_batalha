a92646432b7c4fd8b8262dbc31e1b3ed99ac6cf1
thi4go <thiagofigueiredo777@gmail.com>
2017-07-02 13:01:20 -0300

    correcting function

--- api/controllers/RoundController.js
+++ api/controllers/RoundController.js
@@ -23,24 +23,22 @@ const RoundController = {
 
     getRoundById(round_id){
 		return new Promise( (resolve, reject) => {
-
-			// Round.findById(round_id, function(err, doc) {
-			// 	if (err) reject(err)
-			// 	resolve(doc)
-			// })
-
-            Round.findOne({_id: round_id})
-                 .populate('first second third winner')
-                 .exec(function(err, doc) {
-                     if(err) controller.returnResponseError(res,err);
-                     controller.returnResponseSuccess(res, doc, 'Round populated');
-                  })
+			Round.findById(round_id, function(err, doc) {
+				if (err) reject(err)
+				resolve(doc)
+			})
 		})
     },
 
     _getRoundById : function(req, res, next){
-        let id = req.params.round_id
-        controller.getById(Round, id, req, res, next)
+        let round_id = req.params.round_id
+        
+        Round.findOne({_id: round_id})
+             .populate('first second third winner')
+             .exec(function(err, doc) {
+                 if(err) controller.returnResponseError(res,err);
+                 controller.returnResponseSuccess(res, doc, 'Round populated');
+              })
     },
 
     _getAllRounds : function(req, res, next) {
