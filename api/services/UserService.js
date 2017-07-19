f4f9c2354499e24a40263b487865143c997154b5
vctt94 <victor.duarte.oliveira@gmail.com>
2017-07-18 21:20:32 -0300

    removing code

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -11,36 +11,7 @@ const UserService = {
   updateUserById(id,user){
     return service.updateById(User,id,user)
   },
-  addNewCheckout(user_id, checkout_id){
-    checkoutService = require('./CheckoutService')
-    return new Promise((fulfill,reject)=> {
-      this.getUserById(user_id).then(user=>{
-        checkoutService.getCheckoutById(checkout_id).then(checkout=>{
-          user._checkouts.push(checkout)
-          fulfill(user.save())
-        }).catch(err=>{
-          reject(err)
-        })
-      }).catch(err=>{
-        reject(err)
-      })
-    })
-  },
-  getCheckoutsByUserId(id){
-    return new Promise((fulfill,reject)=> {
-      User.findOne({_id: id})
-        .populate({
-          path: '_checkouts',
-          // Get friends of friends - populate the 'friends' array for every friend
-          populate: { path: '_transaction _user_products' }
-        })
-        .exec(function (err, user) {
-          if (err)
-            reject(err)
-          fulfill(user._checkouts)
-        })
-    })
-  }
+
 
 }
 
